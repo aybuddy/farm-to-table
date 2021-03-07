d8468b7176cb6e672ad3d803020d350a3efba966
Aaron Yu <ayybuddyy@gmail.com>
2021-03-06 21:34:03 -0500

    Finish navbar styles, import icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,39 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
 import { Link, NavLink } from 'react-router-dom';
-import { Nav, NavItem, NavList, NavLogo } from './Navbar.styles';
+import {
+  MobileNavIcon,
+  Nav,
+  NavbarContainer,
+  NavItem,
+  NavList,
+  NavLogo,
+} from './Navbar.styles';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
-        <NavLogo to='/'>MRPX</NavLogo>
-        <NavList>
-          <NavItem>
-            <NavLink to='/menu'>Menu</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to='/about'>About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to='/contact'>Contact</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink to='/order'>Order</NavLink>
-          </NavItem>
-        </NavList>
+        <NavbarContainer>
+          <NavLogo to='/'>MRPX</NavLogo>
+          <NavList>
+            <NavItem>
+              <NavLink to='/menu'>Menu</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/about'>About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/contact'>Contact</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to='/order'>Order</NavLink>
+            </NavItem>
+          </NavList>
+          <MobileNavIcon>
+            <FaBars />
+          </MobileNavIcon>
+        </NavbarContainer>
       </Nav>
     </>
   );
--- src/components/Navbar.styles.js
+++ src/components/Navbar.styles.js
@@ -4,22 +4,32 @@ import { Link } from 'react-router-dom';
 export const Nav = styled.nav`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   width: 100%;
   padding: 12px 24px;
 `;
 
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
 export const NavLogo = styled(Link)`
   cursor: pointer;
+  font-size: 36px;
+  flex: 0.2;
+  text-decoration: none;
 `;
 
 export const NavList = styled.ul`
   display: flex;
-  list-style: none;
   align-items: center;
   justify-content: space-evenly;
   flex: 0.8;
   width: 100%;
+  text-align: center;
 `;
 
 export const NavItem = styled.li`
@@ -28,4 +38,10 @@ export const NavItem = styled.li`
 
 export const NavLink = styled(Link)`
   text-decoration: none;
+  display: flex;
+  align-items: center;
+`;
+
+export const MobileNavIcon = styled.div`
+  display: block;
 `;
